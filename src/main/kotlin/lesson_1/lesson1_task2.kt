ce24c41138f331763fdc0f61eef661a2b760fdf7
Алексей <mr-kaifat@yandex.ru>
2025-06-07 17:48:28 +0300

    KS-1-2 completed

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main(){
+    val totalOrders: Int = 75
+    val orderReview: String = "Thank you for your purchase, come visit us again."
+    println("$totalOrders, $orderReview")
+    var numberOfEmployees: Int = 2000
+    //println(numberOfEmployees)
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
