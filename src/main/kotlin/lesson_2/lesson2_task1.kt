70a2d47b20f856d9eb877121780954bb2195b8c6
Алексей <mr-kaifat@yandex.ru>
2025-06-08 14:42:54 +0300

    KS-2-1 fixed

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -4,6 +4,6 @@ const val NUMBER_OF_STUDENTS = 4
 
 fun main(){
     val studentsScore = listOf(3,4,3,5)
-    val averageScore = studentsScore.sum().toFloat()/NUMBER_OF_STUDENTS
+    val averageScore = studentsScore.sum().toFloat() / NUMBER_OF_STUDENTS
     println(averageScore)
 }
