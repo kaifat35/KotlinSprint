220006937351760eb64ba83701f5f70a811278ba
Алексей <mr-kaifat@yandex.ru>
2025-06-09 15:52:55 +0300

    KS-3-4 completed

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main(){
+    var wereFrom = "E2"
+    var where = "E4"
+    var strokeNumber = 1
+    println("[$wereFrom - $where; $strokeNumber]")
+    wereFrom = "D2"
+    where = "D3"
+    strokeNumber = 2
+    println("[$wereFrom - $where; $strokeNumber]")
+}
