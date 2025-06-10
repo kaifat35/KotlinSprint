ed802cada3ed89304be6a7749489b4f8efe61936
Алексей <mr-kaifat@yandex.ru>
2025-06-10 13:45:42 +0300

    KS-4-1 completed

---
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_4
+
+const val TABLES = 13
+
+fun main(){
+    val reservedTablesToday = 13
+    val reservedTablesTomorrow = 9
+    val availabilityTablesToday = reservedTablesToday != TABLES
+    val availabilityTablesTomorrow = reservedTablesTomorrow != TABLES
+    println("Доступность столиков на сегодня: $availabilityTablesToday\nДоступность столиков на завтра: $availabilityTablesTomorrow")
+}
