4d082d61457387f4f0d8677a9bc177ce0ccb9201
Алексей <mr-kaifat@yandex.ru>
2025-06-10 11:51:35 +0300

    KS-2-5 fixed

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -4,8 +4,9 @@ import kotlin.math.pow
 
 fun main(){
     val inputSumUser = 70000
-    val interestRate = 0.167
+    val interestRate = 16.7
+    val totalRate = 100
     val depositTimeOld = 20
-    val totalAmount = inputSumUser * (1 + interestRate / 1).pow(depositTimeOld)
+    val totalAmount = inputSumUser * (1 + interestRate / totalRate * 1).pow(depositTimeOld)
     println("%.3f".format(totalAmount))
 }
