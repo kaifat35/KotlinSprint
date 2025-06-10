fab617e7d4b7eddb37e34991e6fefd9e34611dee
Алексей <mr-kaifat@yandex.ru>
2025-06-10 11:44:56 +0300

    KS-2-4 fixed

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_2
 
-const val BUFF = 0.2
+const val BUFF_PERCENT = 20
+const val TOTAL_PERCENT = 100
 
 fun main(){
     val ironOre = 11
     val crystalOre = 7
-    val ironOreBuff = (ironOre * BUFF).toInt()
-    val crystalOreBuff = (crystalOre * BUFF).toInt()
+    val ironOreBuff = ironOre * BUFF_PERCENT / TOTAL_PERCENT
+    val crystalOreBuff = crystalOre * BUFF_PERCENT / TOTAL_PERCENT
     println("Iron Ore Buff: $ironOreBuff")
     println("Crystal Ore Buff: $crystalOreBuff")
 }
