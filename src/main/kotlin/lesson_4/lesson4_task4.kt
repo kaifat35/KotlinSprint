7343bc422c57e937bbd4e7c1b67e7d0a9ff99418
Алексей <mr-kaifat@yandex.ru>
2025-06-11 13:27:14 +0300

    KS-4-4 fixed

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,15 +1,14 @@
 package org.example.lesson_4
 
-const val DAY_START = 1
-
-fun main(){
+fun main() {
     val dayNow = 5
-    val trainingArmsPress = dayNow % 2 == DAY_START
-    val trainingLegsBack = !trainingArmsPress
-    println("""
-        Упражнения для рук:    $trainingArmsPress
-        Упражнения для ног:    $trainingLegsBack
-        Упражнения для спины:  $trainingLegsBack
-        Упражнения для пресса: $trainingArmsPress
-    """.trimIndent())
+    val isTrainingDay = dayNow % 2 == 0
+    println(
+        """
+        Упражнения для рук:    ${!isTrainingDay}
+        Упражнения для ног:    $isTrainingDay
+        Упражнения для спины:  $isTrainingDay
+        Упражнения для пресса: ${!isTrainingDay}
+    """.trimIndent()
+    )
 }
