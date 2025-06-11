88a3d4a7afbb308259fe34fd99b6e4fd3a71845f
Алексей <mr-kaifat@yandex.ru>
2025-06-11 16:36:58 +0300

    KS-5-1 completed

---
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_5
+
+fun main() {
+    print("Сложи 2+2=")
+    val input = readln().toInt()
+    if (input == 4) {
+        print("Добро пожаловать!")
+    } else {
+        print("Доступ запрещен.")
+    }
+}
