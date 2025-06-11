4e7bfad323192350518c5eae700b0876fb765bc7
Алексей <mr-kaifat@yandex.ru>
2025-06-11 13:10:25 +0300

    KS-4-2 fixed

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,20 +1,28 @@
 package org.example.lesson_4
 
-const val WEIGHT_MIN_AVERAGE = 36
+const val WEIGHT_MIN_AVERAGE = 35
 const val WEIGHT_MAX_AVERAGE = 100
-const val VOLUME_MIN_AVERAGE = 0
-const val VOLUME_MAX_AVERAGE = 99
+const val VOLUME_MAX_AVERAGE = 100
 
-fun main(){
-    val weightCargo1= 20
+fun main() {
+    val weightCargo1 = 20
     val volumeCargo1 = 80
-    val weightCargo2= 50
+    val weightCargo2 = 50
     val volumeCargo2 = 100
-
-    println("Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории 'Average':" +
-            " ${WEIGHT_MIN_AVERAGE <= weightCargo1 && weightCargo1 <= WEIGHT_MAX_AVERAGE 
-                    && VOLUME_MIN_AVERAGE <= volumeCargo1 && volumeCargo1 <= VOLUME_MAX_AVERAGE}")
-    println("Груз с весом $weightCargo2 кг и объемом $volumeCargo2 л соответствует категории 'Average':" +
-            " ${WEIGHT_MIN_AVERAGE <= weightCargo2 && weightCargo2 <= WEIGHT_MAX_AVERAGE
-                    && VOLUME_MIN_AVERAGE <= volumeCargo2 && volumeCargo2 <= VOLUME_MAX_AVERAGE}")
+    println(
+        "Груз с весом $weightCargo1 кг и объемом $volumeCargo1 л соответствует категории 'Average':" +
+                " ${
+                    weightCargo1 > WEIGHT_MIN_AVERAGE &&
+                            weightCargo1 <= WEIGHT_MAX_AVERAGE &&
+                            volumeCargo1 < VOLUME_MAX_AVERAGE
+                }"
+    )
+    println(
+        "Груз с весом $weightCargo2 кг и объемом $volumeCargo2 л соответствует категории 'Average':" +
+                " ${
+                    weightCargo2 > WEIGHT_MIN_AVERAGE &&
+                            weightCargo2 <= WEIGHT_MAX_AVERAGE &&
+                            volumeCargo2 < VOLUME_MAX_AVERAGE
+                }"
+    )
 }
