322ee84fbbbd8e86fd2043ecf2afac4fec6346e3
Алексей <mr-kaifat@yandex.ru>
2025-06-12 09:08:45 +0300

    KS-4-5 fixed

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -5,16 +5,21 @@ const val MAX_CREW = 70
 const val MIN_PROVISIONS = 50
 
 fun main() {
-    val input = readLine() ?: return
+    println(
+        "Введите через пробел: наличие повреждений корпуса,\n" +
+                "состав экипажа, количество ящиков с провизией,\n" +
+                "благоприятность погоды."
+    )
+    val input = readln()
     val result = input.split(" ")
-    val isDamage = result[0].toBoolean()
+    val isDamaged = result[0].toBoolean()
     val crew = result[1].toInt()
     val provisions = result[2].toInt()
-    val isWeatherConditions = result[3].toBoolean()
+    val isWeatherGood = result[3].toBoolean()
 
-    val isSwimming = (!isDamage && crew in MIN_CREW..MAX_CREW &&
+    val isSwimming = (!isDamaged && crew in MIN_CREW..MAX_CREW &&
             provisions > MIN_PROVISIONS) ||
-            (isDamage && crew == MAX_CREW &&
-                    provisions >= MIN_PROVISIONS && isWeatherConditions)
+            (isDamaged && crew == MAX_CREW &&
+                    provisions >= MIN_PROVISIONS && isWeatherGood)
     println("Корабль может отправиться в плавание: $isSwimming")
 }
