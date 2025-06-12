c792dbf3b3d0f0b2a0bdf97163a533e1fc733976
Алексей <mr-kaifat@yandex.ru>
2025-06-12 13:31:51 +0300

    KS-5-1 fixed

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,11 +1,11 @@
 package org.example.lesson_5
 
 fun main() {
-    val a = 2
-    val b = 2
-    print("Введи результат сложения: $a + $b = ")
+    val firstNumber = 2
+    val secondNumber = 2
+    print("Введи результат сложения: $firstNumber + $secondNumber = ")
     val input = readln().toInt()
-    if (input == a + b) {
+    if (input == firstNumber + secondNumber) {
         print("Добро пожаловать!")
     } else {
         print("Доступ запрещен.")
